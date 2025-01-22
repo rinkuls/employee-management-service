67e05dc180f5dabd317ba40cdf773d1e1116fc01
rinkul.sharma <rinkul.sharma@accenture.com>
2025-01-22 12:39:11 +0100

    Your commit message describing the changes

--- employee-management-tool-ui/src/environments/environment.prod.ts
+++ employee-management-tool-ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  prod: true,
+  production: true,
     API_URL_AUTH: 'http://authentication-service:8099',
     API_URL_EMPLOYEE: 'http://employee-management-tool:8071',
   };
